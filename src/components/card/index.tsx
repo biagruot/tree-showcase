5f611b55140e34a766b2e3ed5a2771fb1392fe71
Biagio Ruotolo <biagio.ruotolo@aboutyou.com>
2023-01-11 00:57:11 +0100

    perf: fix skeleton not loading due to NextJS issue
    - It seems that NextJS currently has a bug with the blur placeholder image that is not loaded
    - Manually added the shimmer converted to Base64 as the NextJS 13 official documentation suggest

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -7,6 +7,25 @@ type Props = {
   image: string;
 };
 
+const shimmer = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`;
+
+const toBase64 = (str: string) =>
+  typeof window === `undefined`
+    ? Buffer.from(str).toString(`base64`)
+    : window.btoa(str);
+
 export const Card: FC<Props> = ({ name, species, image }) => {
   const [showImage, setShowImage] = useState(false);
 
@@ -34,7 +53,9 @@ export const Card: FC<Props> = ({ name, species, image }) => {
           alt={`Picture of a ${name} tree`}
           className="rounded-lg mt-4"
           placeholder="blur"
-          blurDataURL="/skeleton.svg"
+          blurDataURL={`data:image/svg+xml;base64,${toBase64(
+            shimmer(240, 240),
+          )}`}
         />
       </div>
 
